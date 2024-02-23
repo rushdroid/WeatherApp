95ad181baeb6bc17defa8c0320be3880fc2575aa
Rushabh Prajapati <rushabhprajapati20@gmail.com>
2024-02-23 16:52:32 +0530

    minor fixes

--- app/src/main/java/com/example/weatherappdemo/util/Constants.kt
+++ app/src/main/java/com/example/weatherappdemo/util/Constants.kt
@@ -2,6 +2,6 @@ package com.example.weatherappdemo.util
 
 object Constants {
     val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    val API_KEY = "21024bdfa828396d098236b0e6254f9f" // Replace your api key
+    val API_KEY = "REPLACE_YOUR_API_KEY" // Replace your api key
     val COUNTRY_CODE = "IN" // Replace country code for which you want to get data
 }
